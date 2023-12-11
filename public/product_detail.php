43141265d5903f6ec4d6fc4b4733e4d65cbdf0ef
Agrani <agrani58@gmail.com>
2023-12-11 20:44:23 +0545

    updated the product_detail and index page. sellerpage has problems

--- public/product_detail.php
+++ public/product_detail.php
@@ -2,14 +2,6 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>ePasal -Product details</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" />
-    <link rel="stylesheet" href="css/style.css" />
-</head>
-
-<head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ePasal - Home Page</title>
@@ -17,85 +9,81 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-        href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&family=Roboto+Condensed:wght@300&display=swap"
-        rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&family=Roboto+Condensed:wght@300&display=swap" rel="stylesheet" />
     <link rel="stylesheet" href="css/style.css">
 </head>
 <!-- Header Start -->
-<?php 
-   include_once("./partials/header.php");
-    ?>
+<?php
+include_once("./partials/header.php");
+?>
 
 <div class="product-detail">
-    
-
     <div class="container">
         <div class="row">
             <div class="col-7">
-                <div class="product-info>">
-                    <img src="/epasale/public/img/products/biryani.jpg" class="img" />
-                    <div class="product-info">
+                <div class="product-info">
+                    <img src="/epasale/public/img/products/biryani.jpg" class="product_img" />
+                    <div class="product-details">
                         <h3 class="product-name">Chicken Biryani</h3>
                         <P class="discription">Price: Rs.300</P>
                         <div class="quantity">
                             <p>Quantity</p>
-                            <button class="btn">-</button>
-                            <div class="no.">1</div>
-                            <button class="btn">+</button>
+                            <button class="button">-</button>
+                            <div class="number">1</div>
+                            <button class="button">+</button>
                         </div>
-                        <div class="btn2">
+                        <div class="button2">
                             <button class="buy">Buy Now</button>
                             <button class="cart">Add To Cart</button>
                         </div>
-                        <p class="detail">Product Details</p>
-                        <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </li>
-    
+                        <div class="detail">Product Details
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p>
+                        </div>
                     </div>
                 </div>
             </div>
-          <div class="col-5">
-            <div class="product-recommendation">
-                <div class="border">
-                    <p class="store">Product Recommendations</p>
-                </div>
-                <div class="product">
-                    <img src="/epasale/public/img/products/C Momo.jpeg" class="pic" />
-                    <div class="product-info">
-                        <h3 class="product-name">MoMo</h3>
-                        <P class="discription">Price: Rs 200</P>
+            <div class="col-5">
+                <div class="product-recommendation">
+                    <div class="border">
+                        <p class="store">Product Recommendations</p>
+                        </div>
+                    <div class="seller_product">
+                        <img src="/epasale/public/img/products/C Momo.jpeg" class="sideproduct_img" />
+                        <div class="product-info">
+                            <h3 class="product-name">MoMo</h3>
+                            <P class="discription">Price: Rs 200</P>
+                        </div>
                     </div>
-                </div>
-                <div class="product">
-                    <img src="/epasale/public/img/products/cheese pizza.jpg" class="pic" />
-                    <div class="product-info">
-                        <h3 class="product-name">Pizza</h3>
-                        <P class="discription">Price: Rs .250</P>
+                    <div class="seller_product">
+                        <img src="/epasale/public/img/products/cheese pizza.jpg" class="sideproduct_img" />
+                        <div class="product-info">
+                            <h3 class="product-name">Pizza</h3>
+                            <P class="discription">Price: Rs .250</P>
+                        </div>
                     </div>
-                </div>
-                <div class="product">
-                    <img src="/epasale/public/img/products/Jalebi.jpg" class="pic" />
-                    <div class="product-info">
-                        <h3 class="product-name">Burger</h3>
-                        <P class="discription">Price: Rs.200</P>
+                    <div class="seller_product">
+                        <img src="/epasale/public/img/products/Jalebi.jpg" class="sideproduct_img" />
+                        <div class="product-info">
+                            <h3 class="product-name">Burger</h3>
+                            <P class="discription">Price: Rs.200</P>
+                        </div>
                     </div>
-                </div>
-                <div class="product">
-                    <img src="/epasale/public/img/products/Chocolate Milk Shake.jpg" class="pic" />
-                    <div class="product-info">
-                        <h3 class="product-name">Coffee</h3>
-                        <p class="discription">Price: Rs.150</p>
+                    <div class="seller_product">
+                        <img src="/epasale/public/img/products/Chocolate Milk Shake.jpg" class="sideproduct_img" />
+                        <div class="product-info">
+                            <h3 class="product-name">Coffee</h3>
+                            <p class="discription">Price: Rs.150</p>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
         </div>
     </div>
 </div>
 
-<?php 
-   include_once("./partials/footer.php");
-    ?>
+<?php
+include_once("./partials/footer.php");
+?>
 </body>
 
 </html>
---
+++ public/sellerpage.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ePasal - seller Page</title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&family=Roboto+Condensed:wght@300&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<!-- seller page -->
+<?php
+include_once("./partials/header.php");
+?>
+
+<div class="container">
+    <div class="sellerfullpage">
+        <div class="seller_detail">
+            <div class="sp-detail">
+                <img src="/epasale/public/img/products/biryani.jpg" alt="logo">
+            </div>
+            <form action="" method="get">
+                <label for="name">name </label>
+                <input id="name" type="text">
+
+                <label for="address">address:</label>
+                <input id="address" type="text">
+
+                <label for="contact">contact:</label>
+                <input id="contact" type="text">
+
+                <button type="submit" name="Submit_name">SEE MORE</button>
+            </form>
+        </div>
+    
+            <div class="sellerpage">
+                <div class="seller_content">
+                    <img src="/epasale/public/img/products/veg_momo.jpg" alt="Productimage">
+                    <h3>Chowmein</h3>
+                    <p>NRs:100</p>
+
+                    <div class="sp_quantity_buttons">Qty:
+                        <button class="button1">-</button>
+                        <div class="number">1</div>
+                        <button class="button2">+</button>
+                    </div>
+
+                    <div class="sp_card__footer"><!-- div for card__footer-->
+                    
+                            <button type="button">Add to Cart</button>
+                        </div><!--action ends-->
+                    </div><!--div for card footer ends-->
+           
+
+               
+            </div>
+        </div>
+    </div>
+    
+    <?php
+    include_once("./partials/footer.php");
+    ?>
+    </body>
+
+</html>
