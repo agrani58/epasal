448bbedaa65203761ed0cd87882c2ece4c60a665
Alisha Pokhrel <alisha.pokhrel021@apexcollege.edu.np>
2023-12-14 13:21:20 +0545

    applied bem to login

--- login.php
+++ login.php
@@ -6,82 +6,51 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Page</title>
 
-    <!-- CSS Stylesheets Start -->
     <link rel="stylesheet" href="/epasale/public/css/style.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+    <link rel="stylesheet" href="./login.css" />
+    <!-- google font start -->
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link
         href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&family=Roboto+Condensed:wght@300&display=swap"
         rel="stylesheet" />
-    <!-- CSS Stylesheets End -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+    <!-- google font end -->
+
+    <!--font aweosome-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+        integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 
 <body>
+    
+    <div class="login-form">
+        <h1 class="login-form__h1">CUSTOMER LOGIN</h1>
 
-    <!-- Includes header partial from ./_header.php -->
-    <?php include_once("_header.php"); ?>
+        <div class="login-form__wrapper">
+            <div class="login-form__form-group">
+                <i class="fa fa-user"></i>
+                <label class="login-form__label" for="username">Username</label>
+                <input class="login-form__input" type="text">
+            </div>
 
-    <div class="container">
-        <p class="paragraph">CUSTOMER LOGIN PAGE</p>
-        <div>
-            <div>
-                <p class="icon"> <i class="fa fa-user"></i></p>
-                <label for="username">Username:</label>
-
-                <label class="position-right" for="forgotpw">Already have an account?
-                    <a href="#">Sign up</a></label>
+            <div class="login-form__form-group">
+                <i class="fa fa-lock"></i>
+                <label class="login-form__label" for="password">Password</label>
+                <input class="login-form__input" type="password">
 
             </div>
 
-            <div>
-                <input class="width-area" type="text" id="username">
-
-            </div>
+            <span class="login-form__form-group">
+                    <!-- Remember me is not required -->
+                    <!-- <p> <input type="checkbox">Remember me </p> -->
+                    <p class="login-form__p"><a href="#">Forgot Password?</a></p>
+            </span>
+            <button class="button info">login <i class="fa fa-arrow-right-to-bracket"></i></button>
         </div>
-
-        <div>
-
-
-            <div>
-                <p class="icon"> <i class="fa fa-lock"></i></p>
-                <label for="password">Password:</label>
-                <label class="position-right show">
-                    <p class="small-icon"> <i class="fa fa-eye"></i> </p>
-                    Show
-                </label>
-
-
-            </div>
-            <div>
-                <input class="width-area" type="text" id="password">
-            </div>
-        </div>
-
-
-
-        <div>
-            <input type="checkbox">
-            <label for="checklist">Remember me </label>
-        </div>
-
-
-
-        <div>
-            <button class="button info" type="login">Login</button>
-        </div>
-
-        <div class="position-center">
-            <label class="position-right" for="fpassword"><a href="#">Forgot Password?</a></label>
-        </div>
-
-
-
-
     </div>
 
-    <!-- Includes footer partial from ./_footer.php -->
-    <?php include_once("_footer.php"); ?>
 </body>
 
 </html>
