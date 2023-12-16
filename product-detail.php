79c3d9397eb1e35ae3a2f7a33a5c45781665f1da
Aashish Maharjan <aashishmaharjan2001@gmail.com>
2023-12-16 05:53:38 +0545

    Refactor css and applied BEM concept

--- product-detail.php
+++ product-detail.php
@@ -16,108 +16,128 @@
     <?php include_once("_header.php"); ?>
 
 
-    <div class="product-detail">
-        <div class="container">
-            <div class="row">
-                <div class="col-7">
-                    <div class="product-detail__product-info">
-                        <img src="/epasale/public/img/products/biryani.jpg" class="product-detail__product-img" />
-                        <div class="product-detail-information">
-                            <h3 class="product-detail-information__product-name__h3">Chicken Biryani</h3>
-                            <P class="product-detail-information__product-name__p">Price: Rs.300</P>
-                            <div class="product-quantity">
-                                <p>Quantity</p>
-                                <button class="product-qtygroup__btn--dec">-</button>
-                                <input value="1" class="product-quantity__button--number" />
-                                <button class="product-qtygroup__btn--inc">+</button>
-                            </div>
-                            <div class="product-detail__group-button">
-                                <button class="button--buy">Buy Now</button>
-                                <button class="seller__btn--cart">Add To Cart</button>
-                            </div>
-                            <div class="product-description">Product Details
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p>
-                            </div>
-                        </div>
+    <div class="just">
+        <div class="prddetail__wrapper">
+            <div class="prddetail">
+                <img src="/epasale/public/img/products/biryani.jpg" class="prddetail__img" />
+                <div class="prddetail__info">
+                    <h3 class="prddetail__infotitle">Chicken Biryani Served with Tomato Sauce</h3>
+                    <h4 class="prddetail__infodesc">Rs.300.00</h4>
+                    <div class="qtygroup">
+                        <button class="qtygroup__btn--dec">-</button>
+                        <input type="number" min="1" max="10" class="qtygroup__input" value="1" size="5" disabled />
+                        <button class="qtygroup__btn--inc">+</button>
+                    </div>
+                    <div class="prddetail__btngroup">
+                        <button class="button btn-primary">Buy Now</button>
+                        <button class="button btn-primary">Add To Cart</button>
+                    </div>
+                    <div class="desc">
+                        <h3>Product Description</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p>
                     </div>
                 </div>
-                <div class="col-5">
-                    <div class="product-recommendation">
+            </div>
 
-                        <div class="product-recommendation__title">
-                            <p class="product-recommendation__title_p">Product Recommendations</p>
-                        </div>
-                        <div class="product-thumbnail">
-                            <img src="/epasale/public/img/products/C Momo.jpeg"
-                                class="product-thumbnail__product-img" />
-                            <div class="product-thumbnail__product-description">
-                                <h3 class="product-thumbnail__product-description__h3">MoMo</h3>
-                                <P class="product-thumbnail__discription__p">Price: Rs 200</P>
-                            </div>
-                        </div>
+            <div class="prdrecom">
+                <h3 class="prdrecom__title">Product Recommendations</h3>
 
-                        <div class="product-thumbnail">
-                            <img src="/epasale/public/img/products/C Momo.jpeg"
-                                class="product-thumbnail__product-img" />
-                            <div class="product-thumbnail__product-description">
-                                <h3 class="product-thumbnail__product-description__h3">MoMo</h3>
-                                <P class="product-thumbnail__discription__p">Price: Rs 200</P>
-                            </div>
-                        </div>
+                <div class="prdthumb">
+                    <a class="prdthumb__a" href="/epasale/product-detail.php?id=1">
+                        <img src="/epasale/public/img/products/C Momo.jpeg" class="prdthumb__img" />
+                    </a>
 
-                        <div class="product-thumbnail">
-                            <img src="/epasale/public/img/products/C Momo.jpeg"
-                                class="product-thumbnail__product-img" />
-                            <div class="product-thumbnail__product-description">
-                                <h3 class="product-thumbnail__product-description__h3">MoMo</h3>
-                                <P class="product-thumbnail__discription__p">Price: Rs 200</P>
-                            </div>
-                        </div>
+                    <div class="prdthumb__desc">
+                        <a class="prdthumb__a" href="/epasale/product-detail.php?id=1">
+                            <h3 class="prdthumb__desctitle">MoMo</h3>
+                        </a>
+                        <P class="prdthumb__desctext">Rs. 200</P>
+                    </div>
+                </div>
 
-                        <div class="product-thumbnail">
-                            <img src="/epasale/public/img/products/C Momo.jpeg"
-                                class="product-thumbnail__product-img" />
-                            <div class="product-thumbnail__product-description">
-                                <h3 class="product-thumbnail__product-description__h3">MoMo</h3>
-                                <P class="product-thumbnail__discription__p">Price: Rs 200</P>
-                            </div>
-                        </div>
+                <div class="prdthumb">
+                    <a class="prdthumb__a" href="/epasale/product-detail.php?id=1">
+                        <img src="/epasale/public/img/products/C Momo.jpeg" class="prdthumb__img" />
+                    </a>
 
-                        <div class="product-thumbnail">
-                            <img src="/epasale/public/img/products/C Momo.jpeg"
-                                class="product-thumbnail__product-img" />
-                            <div class="product-thumbnail__product-description">
-                                <h3 class="product-thumbnail__product-description__h3">MoMo</h3>
-                                <P class="product-thumbnail__discription__p">Price: Rs 200</P>
-                            </div>
-                        </div>
+                    <div class="prdthumb__desc">
+                        <a class="prdthumb__a" href="/epasale/product-detail.php?id=1">
+                            <h3 class="prdthumb__desctitle">MoMo</h3>
+                        </a>
+                        <P class="prdthumb__desctext">Rs. 200</P>
+                    </div>
+                </div>
 
-                        <div class="product-thumbnail">
-                            <img src="/epasale/public/img/products/C Momo.jpeg"
-                                class="product-thumbnail__product-img" />
-                            <div class="product-thumbnail__product-description">
-                                <h3 class="product-thumbnail__product-description__h3">MoMo</h3>
-                                <P class="product-thumbnail__discription__p">Price: Rs 200</P>
-                            </div>
-                        </div>
+                <div class="prdthumb">
+                    <a class="prdthumb__a" href="/epasale/product-detail.php?id=1">
+                        <img src="/epasale/public/img/products/C Momo.jpeg" class="prdthumb__img" />
+                    </a>
 
-                        <div class="product-thumbnail">
-                            <img src="/epasale/public/img/products/C Momo.jpeg"
-                                class="product-thumbnail__product-img" />
-                            <div class="product-thumbnail__product-description">
-                                <h3 class="product-thumbnail__product-description__h3">MoMo</h3>
-                                <P class="product-thumbnail__discription__p">Price: Rs 200</P>
-                            </div>
-                        </div>
+                    <div class="prdthumb__desc">
+                        <a class="prdthumb__a" href="/epasale/product-detail.php?id=1">
+                            <h3 class="prdthumb__desctitle">MoMo</h3>
+                        </a>
+                        <P class="prdthumb__desctext">Rs. 200</P>
+                    </div>
+                </div>
 
+                <div class="prdthumb">
+                    <a class="prdthumb__a" href="/epasale/product-detail.php?id=1">
+                        <img src="/epasale/public/img/products/C Momo.jpeg" class="prdthumb__img" />
+                    </a>
 
+                    <div class="prdthumb__desc">
+                        <a class="prdthumb__a" href="/epasale/product-detail.php?id=1">
+                            <h3 class="prdthumb__desctitle">MoMo</h3>
+                        </a>
+                        <P class="prdthumb__desctext">Rs. 200</P>
+                    </div>
+                </div>
+
+                <div class="prdthumb">
+                    <a class="prdthumb__a" href="/epasale/product-detail.php?id=1">
+                        <img src="/epasale/public/img/products/C Momo.jpeg" class="prdthumb__img" />
+                    </a>
+
+                    <div class="prdthumb__desc">
+                        <a class="prdthumb__a" href="/epasale/product-detail.php?id=1">
+                            <h3 class="prdthumb__desctitle">MoMo</h3>
+                        </a>
+                        <P class="prdthumb__desctext">Rs. 200</P>
+                    </div>
+                </div>
+
+                <div class="prdthumb">
+                    <a class="prdthumb__a" href="/epasale/product-detail.php?id=1">
+                        <img src="/epasale/public/img/products/C Momo.jpeg" class="prdthumb__img" />
+                    </a>
+
+                    <div class="prdthumb__desc">
+                        <a class="prdthumb__a" href="/epasale/product-detail.php?id=1">
+                            <h3 class="prdthumb__desctitle">MoMo</h3>
+                        </a>
+                        <P class="prdthumb__desctext">Rs. 200</P>
+                    </div>
+                </div>
+
+                <div class="prdthumb">
+                    <a class="prdthumb__a" href="/epasale/product-detail.php?id=1">
+                        <img src="/epasale/public/img/products/C Momo.jpeg" class="prdthumb__img" />
+                    </a>
+
+                    <div class="prdthumb__desc">
+                        <a class="prdthumb__a" href="/epasale/product-detail.php?id=1">
+                            <h3 class="prdthumb__desctitle">MoMo</h3>
+                        </a>
+                        <P class="prdthumb__desctext">Rs. 200</P>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
 
-        <!-- Includes footer partial from ./_footer.php -->
-        <?php include_once("_footer.php"); ?>
+    <!-- Includes footer partial from ./_footer.php -->
+    <?php include_once("_footer.php"); ?>
 </body>
 
 </html>
