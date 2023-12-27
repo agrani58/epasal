65b3a4567d81c3d01fc0d78009cfde8c5f639fc1
Agrani <agrani58@gmail.com>
2023-12-27 20:39:30 +0545

    Customer Login Page-agrani

--- sellerlogin.php
+++ sellerlogin.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Page</title>
+    <title>Sign-Up Page</title>
 
     <!-- CSS Stylesheets Start -->
     <link rel="stylesheet" href="/epasale/public/css/style.css">
